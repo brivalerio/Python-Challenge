088bc19dade5ea69acfd4cd0f855a347f027509b
Brian Valerio <brivalerio@gmail.com>
2019-04-30 12:56:34 -0700

    Starting code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,14 @@ budget_data = os.path.join("Resources", "election_data.csv")
 #Variables
 total_votes = 0
 votes = []
+
+
+
+
+
+#Text file output path
+#analysis_text_file = os.path.join("Election Results.txt")
+
+#Creating text file with election results
+#Using \n to clean up and create 9 lines of text in text file otherwise
+#it would show as single line
