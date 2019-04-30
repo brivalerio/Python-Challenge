b987a495ecae6069429d227a898ae00bebfe2c18
Brian Valerio <brivalerio@gmail.com>
2019-04-30 09:42:52 -0700

    Adding more variables for calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,11 @@ budget_data = os.path.join("Resources", "budget_data.csv")
 #Variables
 month_count = 0
 net_total = 0
+month_revenue = 0
+last_month_revenue = 0
+profit_change = 0
+profit_changes = []
+months = []
 
 with open(budget_data,newline="") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -18,9 +23,14 @@ with open(budget_data,newline="") as csv_file:
 
         for csv_header in csv_reader:
                 month_count += 1
-
+                months.append(csv_header[0])
                 net = int(csv_header[1])
                 net_total = net_total + net
+
+                if month_count > 1:
+                        profit_change = month_revenue - last_month_revenue
+                        profit_changes.append(profit_change)
+                last_month_revenue = month_revenue
 
 
 print(f"Total Months: {str(month_count)}")
